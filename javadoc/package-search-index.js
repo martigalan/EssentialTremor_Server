b4a1acccf272fca2b0283e7aa6eff332c091fc0a
Blanca <blancapueche@gmail.com>
2024-11-30 17:22:24 +0100

    javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"iFaces"},{"l":"jdbc"},{"l":"mainServer"},{"l":"pojos"},{"l":"tests"}];updateSearchResults();
